e06302ae860e012ee4d5b6f0a523cb62e1b5c515
Joseph Wright <rjosephwright@gmail.com>
2015-11-19 00:48:40 -0800

    Update to newer nightly and fix due to changes

--- src/AWS.hs
+++ src/AWS.hs
@@ -4,38 +4,36 @@ module AWS where
 import Control.Lens
 import qualified Control.Monad.Trans.AWS as T
 import Data.Text (Text)
-import Network.AWS.EC2.DescribeVpcs
+import Network.AWS.EC2.DescribeVPCs
 import Network.AWS.EC2.DescribeSubnets
 import Network.AWS.EC2.Types
 import Network.AWS
 import System.IO
 
-vpcAttr :: Text -> Getting a Vpc a -> IO (Maybe a)
+vpcAttr :: Text -> Getting a VPC a -> IO (Maybe a)
 vpcAttr vpcId attr = do
     l <- newLogger Info stdout
-    env <- getEnv NorthCalifornia Discover <&> envLogger .~ l
-    r <- T.runAWST env $ do
-      vpcs <- T.send $ describeVpcs & dv1VpcIds .~ [vpcId]
-      return (vpcs^.dvrVpcs)
+    env <- newEnv NorthCalifornia Discover <&> envLogger .~ l
+    r <- T.runResourceT . T.runAWST env $ do
+      vpcs <- T.send $ describeVPCs & dvsVPCIds .~ [vpcId]
+      return (vpcs^.dvrsVPCs)
     case r of
-      (Left _) -> return Nothing
-      (Right []) -> return Nothing
-      (Right (vpc:_)) -> return $ Just (vpc^.attr)
+      [] -> return Nothing
+      (vpc:_) -> return $ Just (vpc^.attr)
 
 cidrBlock :: Text -> IO (Maybe Text)
-cidrBlock vpcId = vpcAttr vpcId vpcCidrBlock
+cidrBlock vpcId = vpcAttr vpcId vpcCIdRBlock
 
 subnetCidrBlocks :: Text -> IO (Maybe [Text])
-subnetCidrBlocks vpcId = subnetsAttr vpcId s1CidrBlock
+subnetCidrBlocks vpcId = subnetsAttr vpcId subCIdRBlock
 
 subnetsAttr :: Text -> Getting a Subnet a -> IO (Maybe [a])
 subnetsAttr vpcId attr = do
   l <- newLogger Info stdout
-  env <- getEnv NorthCalifornia Discover <&> envLogger .~ l
-  r <- T.runAWST env $ do
+  env <- newEnv NorthCalifornia Discover <&> envLogger .~ l
+  r <- T.runResourceT . T.runAWST env $ do
     sn <- T.send $ describeSubnets & dsFilters .~ [filter' "vpc-id" & fValues .~ [vpcId]]
-    return (sn^.dsrSubnets)
+    return (sn^.dsrsSubnets)
   case r of
-    (Left _) -> return Nothing
-    (Right []) -> return Nothing
-    (Right sns) -> return $ Just [sn^.attr | sn <- sns]
+    [] -> return Nothing
+    sns -> return $ Just [sn^.attr | sn <- sns]
